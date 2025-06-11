7c34ed47a247fc93508ddab06bead33a22cdcb46
iahta <153889016+iahta@users.noreply.github.com>
2025-06-10 20:40:30 -0500

    retrieve file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,31 +1,26 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    file_list = []
-    abs_path = os.path.abspath(working_directory)
-    subdir_path = os.path.join(abs_path, directory)
-    dir_abs_path = os.path.abspath(subdir_path)
-    if dir_abs_path.startswith(abs_path) == False:
+    abs_working_path = os.path.abspath(working_directory)
+    target_dir = abs_working_path
+    if directory:
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_path):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if os.path.isdir(dir_abs_path) == False:
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-    files = os.listdir(dir_abs_path)
-    for file in files:
-        try: 
-            file_path = os.path.join(dir_abs_path, file)           
-        except:
-            return "Error: An error occured joining file paths."
-        try:
-            file_size = os.path.getsize(file_path)
-        except:
-            return "Error: An error occured retrieving the file size."
-        try:
-            is_dir = os.path.isdir(file_path)
-        except:
-            return "Error: Unable to determine file type."
-        file_string = f"- {file}: file_size={file_size} bytes, is_dir={is_dir}"
-        file_list.append(file_string)
-    result = "\n".join(file_list)
-    return result
+    try:
+        file_list = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            file_list.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+            return "\n".join(file_list)
+    except Exception as e:
+        return f"Error listing files: {e}"
 
 
--- tests.py
+++ tests.py
@@ -1,37 +1,26 @@
 import unittest
 
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 class TestGetFilesInfo(unittest.TestCase):
     def test_root(self):
-        result = get_files_info("calculator", ".")
-        print("ROOT")
+        result = get_file_content("calculator", "main.py")
+        print("MAIN")
         print(result)
         print("\n")
 
     def test_pkg(self):
-        result = get_files_info("calculator", "pkg")
+        result = get_file_content("calculator", "pkg/calculator.py")
         print("PKG")
         print(result)
         print("\n")
     
     def test_bin(self):
-        result = get_files_info("calculator", "/bin")
+        result = get_file_content("calculator", "/bin/cat")
         print("BIN")
         print(result)
         print("\n")
 
-    def test_error(self):
-        result = get_files_info("calculator", "../")
-        print("ERROR")
-        print(result)
-        print("\n")
-    
-    def test_file(self):
-        result = get_files_info("calculator", "tests.py")
-        print("FILE")
-        print(result)
-        print("\n")
 
 if __name__ == "__main__":
     unittest.main()
